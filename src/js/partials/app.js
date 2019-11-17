ced6961daed80f9d30333f374b2738cab9de65af
webn1nja <kkozhukhov@yandex.ru>
2019-11-17 11:18:56 +0300

    mmenu, css-hamburgers etc

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,20 @@
+$(function() {
 
+
+  $("#my-menu").mmenu({
+    extensions:["widescreen", "theme-black", "effect-menu-slide", "pagedim-black"],
+    navbar:{
+      title:'<img src="/img/logo-1.svg" alt="Салон красоты Смитлер">'
+    },
+    offCanvas: {
+      position: "right"
+    }
+  });
+
+  var api = $("#my-menu").data("mmenu");
+  api.bind("opened", function(){
+    $(".hamburger").addClass("is-active");
+  }).bind("closed",function(){
+    $(".hamburger").removeClass("is-active");
+  })
+});
